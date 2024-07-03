b8391cc0bddef1f867545c83788f91d0cb88024e
Fiddy <thnhlong2112@gmail.com>
2024-07-03 15:20:34 +0700

    fix contract

--- deploy/contracts/NewToken.sol
+++ deploy/contracts/NewToken.sol
@@ -13,14 +13,22 @@ contract NewToken is ERC20Capped,ERC20Burnable{
     // set block
     uint256 public blockReward;
 
+    bool public isActive;
+
     constructor(uint256 cap, uint256 reward) ERC20("SenpaiToken", "ST") ERC20Capped(cap * (10 ** 18)){
         owner = payable(msg.sender);
         _mint(owner, 50000000 * 10 ** 18);
         blockReward = reward * 10 ** 18;
+        isActive = true;
     }
 
     modifier onlyOwner{
-        require(msg.sender == owner, "You dont have ower, only owner can");
+        require(msg.sender == owner, "You don't have permission, only owner can perform this action");
+        _;
+    }
+
+    modifier onlyWhenActive{
+        require(isActive,"Contract is not active");
         _;
     }
 
@@ -28,18 +36,21 @@ contract NewToken is ERC20Capped,ERC20Burnable{
         _mint(block.coinbase, blockReward);
     }
 
-     function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override() {
-        if (from != address(0) && to != block.coinbase && block.coinbase != address(0)) {
-            _mint(block.coinbase, blockReward);
-        }
-        super._beforeTokenTransfer(from, to, amount);
-    }
+    // function _beforeTokenTransfer(address from, address to, uint256 value) internal virtual override(ERC20, ERC20Capped) {
+    //     if (from != address(0) && to != block.coinbase && block.coinbase != address(0)) {
+    //         _minerReward();
+    //     }
+    //     super._beforeTokenTransfer(from, to, value);
+    // }
+
 
     function destroy() public onlyOwner{
-        selfdestruct(owner);
+        // selfdestruct(owner);
+        owner.transfer(address(this).balance);
+        isActive = false;
     }
 
-    function setBlockReward(uint256 reward) public onlyOwner {
+    function setBlockReward(uint256 reward) public onlyOwner  onlyWhenActive{
         blockReward = reward * 10 ** 18;
     }
 
@@ -49,8 +60,7 @@ contract NewToken is ERC20Capped,ERC20Burnable{
         internal
         override(ERC20, ERC20Capped)
     {
-        super._update(from, to, value);
-        
+        super._update(from, to, value); 
     }
 
 
